a31c2b4961eaa0a543b647bc2c64d2edf5fe297d
Mohammed Afeef Hussain <afeef2001kashif@gmail.com>
2025-03-16 20:20:07 +0530

    Added port for renderon

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -465,4 +465,6 @@ def results():
     return render_template('results.html', metrics=metrics)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render-assigned port or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
+
